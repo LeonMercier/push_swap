6f4ef3eb869c2599b36a8afe83552765986515ce
lemercie <lemercie@student.hive.fi>
2024-05-31 17:51:35 +0300

    drafting

---
+++ push_swap.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lemercie <lemercie@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/31 16:39:57 by lemercie          #+#    #+#             */
+/*   Updated: 2024/05/31 17:51:08 by lemercie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+//https://cs.stackexchange.com/questions/90202/sorting-a-stack-using-bubble-sort
+	  
+// read input into array
+//
+// create second array of equal size
+//
+// allocate string for output
+// for performance, we use strlcat to construct the output string
+// rellocate as necessary to grow the space
+//
+// bubble sort
